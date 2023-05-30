ca2df6fcee246e82d4b534bcf6b3bfb03be00ee6
HappyAdonis <trongnp144@gmail.com>
2023-05-30 18:34:19 +0700

    version 1.1

--- money-management-booking/src/main/java/com/group6/moneymanagementbooking/App.java
+++ money-management-booking/src/main/java/com/group6/moneymanagementbooking/App.java
@@ -1,4 +1,5 @@
-package com.group6.moneymanagementbooking.util;
+package com.group6.moneymanagementbooking;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ money-management-booking/src/main/java/com/group6/moneymanagementbooking/model/TokenPayload.java
@@ -0,0 +1,17 @@
+package com.group6.moneymanagementbooking.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenPayload {
+    private int userId;
+    private String email;
+}

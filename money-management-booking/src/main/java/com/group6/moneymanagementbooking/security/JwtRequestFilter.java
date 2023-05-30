7af5033a49ae3b2fe7146acae0ac51682648582f
HappyAdonis <trongnp144@gmail.com>
2023-05-30 20:36:28 +0700

    version 1.0

--- money-management-booking/src/main/java/com/group6/moneymanagementbooking/security/JwtRequestFilter.java
+++ money-management-booking/src/main/java/com/group6/moneymanagementbooking/security/JwtRequestFilter.java
@@ -1,77 +1,77 @@
-package com.group6.moneymanagementbooking.security;
+// package com.group6.moneymanagementbooking.security;
 
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+// import java.io.IOException;
+// import java.util.ArrayList;
+// import java.util.List;
+// import java.util.Optional;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+// import javax.servlet.FilterChain;
+// import javax.servlet.ServletException;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
+// import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+// import org.springframework.security.core.authority.SimpleGrantedAuthority;
+// import org.springframework.security.core.context.SecurityContextHolder;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.stereotype.Component;
+// import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.group6.moneymanagementbooking.enity.User;
-import com.group6.moneymanagementbooking.model.TokenPayload;
-import com.group6.moneymanagementbooking.repository.UserRepository;
-import com.group6.moneymanagementbooking.util.JwtTokenUtil;
+// import com.group6.moneymanagementbooking.enity.User;
+// import com.group6.moneymanagementbooking.model.TokenPayload;
+// import com.group6.moneymanagementbooking.repository.UserRepository;
+// import com.group6.moneymanagementbooking.util.JwtTokenUtil;
 
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.SignatureException;
-import lombok.RequiredArgsConstructor;
+// import io.jsonwebtoken.ExpiredJwtException;
+// import io.jsonwebtoken.SignatureException;
+// import lombok.RequiredArgsConstructor;
 
-@Component
-@RequiredArgsConstructor
-public class JwtRequestFilter extends OncePerRequestFilter {
+// @Component
+// @RequiredArgsConstructor
+// public class JwtRequestFilter extends OncePerRequestFilter {
 
-    private final UserRepository userRepository;
+//     private final UserRepository userRepository;
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        final String requestTokenHeader = request.getHeader("Authorization");
+//     @Override
+//     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+//             throws ServletException, IOException {
+//         final String requestTokenHeader = request.getHeader("Authorization");
 
-        String token = "";
-        TokenPayload tokenPayload = null;
+//         String token = "";
+//         TokenPayload tokenPayload = null;
 
-        if (requestTokenHeader != null && requestTokenHeader.startsWith("Token ")) {
-            token = requestTokenHeader.substring(6).trim();
-            try {
-                tokenPayload = JwtTokenUtil.getTokenPayLoad(token);
-            } catch (SignatureException se) {
-                System.out.println("Invalid JWT signature");
-            } catch (IllegalArgumentException illae) {
-                System.out.println("Unable to get jwt");
-            } catch (ExpiredJwtException exe) {
-                System.out.println("Token has expired");
-            }
-        } else {
-            System.out.println("JWT token does not start with 'Token'");
-        }
-        if (tokenPayload != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            Optional<User> userOptional = userRepository.findById(tokenPayload.getUserId());
+//         if (requestTokenHeader != null && requestTokenHeader.startsWith("Token ")) {
+//             token = requestTokenHeader.substring(6).trim();
+//             try {
+//                 tokenPayload = JwtTokenUtil.getTokenPayLoad(token);
+//             } catch (SignatureException se) {
+//                 System.out.println("Invalid JWT signature");
+//             } catch (IllegalArgumentException illae) {
+//                 System.out.println("Unable to get jwt");
+//             } catch (ExpiredJwtException exe) {
+//                 System.out.println("Token has expired");
+//             }
+//         } else {
+//             System.out.println("JWT token does not start with 'Token'");
+//         }
+//         if (tokenPayload != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+//             Optional<User> userOptional = userRepository.findById(tokenPayload.getUserId());
 
-            if (userOptional.isPresent()) {
-                User user = userOptional.get();
-                if (JwtTokenUtil.validate(token, user)) {
-                    List<SimpleGrantedAuthority> authority = new ArrayList<>();
-                    UserDetails userDetails = new org.springframework.security.core.userdetails.User(user.getEmail(),
-                            user.getPassword(), authority);
-                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-                            userDetails, null, authority);
-                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-                }
-            }
-        }
-        filterChain.doFilter(request, response);
+//             if (userOptional.isPresent()) {
+//                 User user = userOptional.get();
+//                 if (JwtTokenUtil.validate(token, user)) {
+//                     List<SimpleGrantedAuthority> authority = new ArrayList<>();
+//                     UserDetails userDetails = new org.springframework.security.core.userdetails.User(user.getEmail(),
+//                             user.getPassword(), authority);
+//                     UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+//                             userDetails, null, authority);
+//                     SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+//                 }
+//             }
+//         }
+//         filterChain.doFilter(request, response);
 
-    }
+//     }
 
-}
+// }

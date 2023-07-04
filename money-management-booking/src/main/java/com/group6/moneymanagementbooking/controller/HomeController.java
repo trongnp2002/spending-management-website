54b267d6bda428b1a39bcb9ae5c00371c1af1258
bun135 <phuonglvhhe161304@fpt.edu.vn>
2023-07-04 23:23:44 +0700

    home

--- money-management-booking/src/main/java/com/group6/moneymanagementbooking/controller/HomeController.java
+++ money-management-booking/src/main/java/com/group6/moneymanagementbooking/controller/HomeController.java
@@ -1,12 +1,13 @@
 package com.group6.moneymanagementbooking.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 
 
-
+@Controller
 public class HomeController {
         @GetMapping("/homepage")
     public String homepag(Model model)  {
